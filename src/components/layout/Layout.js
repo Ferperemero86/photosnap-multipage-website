60a66914837fab87bda225087a10945d821cf965
Ferperemero86 <fernandoperezmerono@gmail.com>
2021-05-19 23:46:27 +0100

    husky test

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import MainMenu from "./MainMenu";
 import Header from "./Header";
-import Footer from "./Footer";
+import Footer from "./Footer';
 
 const Layout = ({ children }) => {
 	return (
