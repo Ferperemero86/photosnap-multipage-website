383d211402810240bf3c83f36415c9d478e7d221
Ferperemero86 <fernandoperezmerono@gmail.com>
2021-05-20 00:05:26 +0100

    Add prettier to eslint

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 const test = "test";
 
 const Footer = () => {
-	return <h2>{test}</h2>;
+	return <h2>{ test }</h2>;
 };
 
 export default Footer;
--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,8 +1,8 @@
 import React from "react";
 
 import MainMenu from "./MainMenu";
-import Header from "./Header';
-import Footer from "./Footer';
+import Header from "./Header";
+import Footer from "./Footer";
 
 const Layout = ({ children }) => {
 	return (
