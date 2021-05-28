3633da7a2e154521c60469b8ee58611d670e300d
Ferperemero86 <fernandoperezmerono@gmail.com>
2021-05-28 11:12:53 +0100

    Reformat data structure

--- db/data.js
+++ db/data.js
@@ -1,12 +1,518 @@
-const books = [
-	{
-		title: "The Awakening",
-		author: "Kate Chopin"
+const data = {
+	logos: {
+		mainLogo: {
+			url: "https://res.cloudinary.com/dby4kdmbv/image/upload/v1621241087/photosnap/shared/desktop/ykufpoveea1twje8dyvx.svg"
+		},
+		logoSecond: {
+			url: "https://res.cloudinary.com/dby4kdmbv/image/upload/v1621865058/photosnap/shared/desktop/sbog1hoc0kfbbr5rl90f.svg"
+		}
 	},
-	{
-		title: "City of Glass",
-		author: "Paul Auster"
-	}
-];
+	links: {
+		socialLinks: [
+			{
+				linkUrl: "#",
+				imgUrl:
+					"https://res.cloudinary.com/dby4kdmbv/image/upload/v1621241087/photosnap/shared/desktop/o0starsmggmi9mveyw5m.svg"
+			},
+			{
+				linkUrl: "#",
+				imgUrl:
+					"https://res.cloudinary.com/dby4kdmbv/image/upload/v1621241090/photosnap/shared/desktop/vxp727qvzhlpixlbptey.svg"
+			},
+			{
+				linkUrl: "#",
+				imgUrl:
+					"https://res.cloudinary.com/dby4kdmbv/image/upload/v1621241088/photosnap/shared/desktop/rk7izuncdocc1oxapwwa.svg"
+			},
+			{
+				linkUrl: "#",
+				imgUrl:
+					"https://res.cloudinary.com/dby4kdmbv/image/upload/v1621241087/photosnap/shared/desktop/tdki3vk4adp1z3giduxp.svg"
+			}
+		],
+		mainLinks: [
+			{ url: "/", label: "home" },
+			{ url: "/stories", label: "stories" },
+			{ url: "/features", label: "features" },
+			{ url: "/pricing", label: "pricing" }
+		]
+	},
+	icons: {
+		arrowIconFirst: {
+			url: "https://res.cloudinary.com/dby4kdmbv/image/upload/v1621757167/photosnap/shared/desktop/nu9aotbfmqap9mnr2lfd.svg"
+		}
+	},
+	buttons: {
+		invite: { btnLabel: "GET AN INVITE" },
+		story: { btnLabel: "READ STORY" }
+	},
+	cards: {
+		headerPrimary: {
+			heading: "CREATE AND SHARE YOUR PHOTO STORIES.",
+			text: "Photosnap is a platform for photographers and visual storytellers. We make it easy to share photos, tell stories and connect with others.",
+			images: [
+				{
+					size: "sm",
+					url: "https://res.cloudinary.com/dby4kdmbv/image/upload/v1621241058/photosnap/home/mobile/rydd2dkx3ullwumtst23.jpg"
+				},
+				{
+					size: "md",
+					url: "https://res.cloudinary.com/dby4kdmbv/image/upload/v1621241063/photosnap/home/tablet/rqkfw20xkjhndgqztcvz.jpg"
+				},
+				{
+					size: "lg",
+					url: "https://res.cloudinary.com/dby4kdmbv/image/upload/v1621241064/photosnap/home/desktop/hfqazblnokdxsvncjjmy.jpg"
+				}
+			]
+		},
+		headerSecond: {
+			backgroundImages: [
+				{
+					size: "sm",
+					url: "https://res.cloudinary.com/dby4kdmbv/image/upload/v1621241116/photosnap/stories/mobile/ppmvsg2qv3shfrtakxrs.jpg"
+				},
+				{
+					size: "md",
+					url: "https://res.cloudinary.com/dby4kdmbv/image/upload/v1621241124/photosnap/stories/tablet/eul20rkejyemrziqjiwm.jpg"
+				},
+				{
+					size: "lg",
+					url: "https://res.cloudinary.com/dby4kdmbv/image/upload/v1621241109/photosnap/stories/desktop/e84zrlgi0rhmgwohiegc.jpg"
+				}
+			],
+			heading: "CREATE AND SHARE YOUR PHOTO STORIES.",
+			text: "Photosnap is a platform for photographers and visual storytellers. We make it easy to share photos, tell stories and connect with others.",
+			images: []
+		},
+		headerThird: {
+			images: [
+				{
+					size: "sm",
+					url: "https://res.cloudinary.com/dby4kdmbv/image/upload/v1621241046/photosnap/features/mobile/fnu0pm66df8lfxpgwdod.jpg"
+				},
+				{
+					size: "md",
+					url: "https://res.cloudinary.com/dby4kdmbv/image/upload/v1621241050/photosnap/features/tablet/ehuznffrmcnxktabllzv.jpg"
+				},
+				{
+					size: "lg",
+					url: "https://res.cloudinary.com/dby4kdmbv/image/upload/v1621241045/photosnap/features/desktop/t4jmhzzglyxskjk58sgq.jpg"
+				}
+			],
+			heading: "CREATE AND SHARE YOUR PHOTO STORIES.",
+			text: "Photosnap is a platform for photographers and visual storytellers. We make it easy to share photos, tell stories and connect with others."
+		},
+		group1: [
+			{
+				heading: "BEAUTIFUL STORIES EVERY TIME",
+				text: "We provide design templates to ensure your stories look terrific. Easily add photos, text, embed maps and media from other networks. Then share your story with everyone.",
+				images: [
+					{
+						size: "sm",
+						url: "https://res.cloudinary.com/dby4kdmbv/image/upload/v1621241060/photosnap/home/mobile/otw1omtz4ynvsb3dnd3x.jpg"
+					},
+					{
+						size: "md",
+						url: "https://res.cloudinary.com/dby4kdmbv/image/upload/v1621241066/photosnap/home/tablet/tin3whur8m7imucsudsq.jpg"
+					},
+					{
+						size: "lg",
+						url: "https://res.cloudinary.com/dby4kdmbv/image/upload/v1621241065/photosnap/home/desktop/y2bpo2t3s5tbfl7ki6lg.jpg"
+					}
+				]
+			},
+			{
+				heading: "DESIGNED FOR EVERYONE",
+				text: "Photosnap can help you create stories that resonate with your audience.  Our tool is designed for photographers of all levels, brands, businesses you name it.",
+				images: [
+					{
+						size: "sm",
+						url: "https://res.cloudinary.com/dby4kdmbv/image/upload/v1621241065/photosnap/home/mobile/tssnsjvkhvg4ybrzmepa.jpg"
+					},
+					{
+						size: "md",
+						url: "https://res.cloudinary.com/dby4kdmbv/image/upload/v1621241068/photosnap/home/tablet/qr1ucvpf56rbaezivvjf.jpg"
+					},
+					{
+						size: "lg",
+						url: "https://res.cloudinary.com/dby4kdmbv/image/upload/v1621241063/photosnap/home/desktop/cyzoeoodjzhpdrkzymx1.jpg"
+					}
+				]
+			}
+		],
 
-module.exports = books;
+		group2: [
+			{
+				heading: "The Mountains",
+				text: "by John Applessed",
+				images: [
+					{
+						size: "sm",
+						url: "https://res.cloudinary.com/dby4kdmbv/image/upload/v1621241122/photosnap/stories/mobile/izq8uxwmvmazgyuv7yky.jpg"
+					},
+					{
+						size: "md",
+						url: "https://res.cloudinary.com/dby4kdmbv/image/upload/v1621241105/photosnap/stories/desktop/hzxyuk8mgzeo67ukclts.jpg"
+					},
+					{
+						size: "lg",
+						url: "https://res.cloudinary.com/dby4kdmbv/image/upload/v1621241105/photosnap/stories/desktop/hzxyuk8mgzeo67ukclts.jpg"
+					}
+				]
+			},
+			{
+				heading: "Sunset Cityscapes",
+				text: "by Benjamin Cruz",
+				images: [
+					{
+						size: "sm",
+						url: "https://res.cloudinary.com/dby4kdmbv/image/upload/v1621241115/photosnap/stories/mobile/z4hzkkdvmo9fui2e46ow.jpg"
+					},
+					{
+						size: "md",
+						url: "https://res.cloudinary.com/dby4kdmbv/image/upload/v1621241101/photosnap/stories/desktop/d4qptt5wwy24ldifb6ra.jpg"
+					},
+					{
+						size: "lg",
+						url: "https://res.cloudinary.com/dby4kdmbv/image/upload/v1621241101/photosnap/stories/desktop/d4qptt5wwy24ldifb6ra.jpg"
+					}
+				]
+			},
+			{
+				heading: "18 Days Voyage",
+				text: "by Alexei Borodin",
+				images: [
+					{
+						size: "sm",
+						url: "https://res.cloudinary.com/dby4kdmbv/image/upload/v1621241112/photosnap/stories/mobile/os55hmmtbfqvo99dj3un.jpg"
+					},
+					{
+						size: "md",
+						url: "https://res.cloudinary.com/dby4kdmbv/image/upload/v1621241096/photosnap/stories/desktop/tcunkdhd8r9sipes5hcx.jpg"
+					},
+					{
+						size: "lg",
+						url: "https://res.cloudinary.com/dby4kdmbv/image/upload/v1621241096/photosnap/stories/desktop/tcunkdhd8r9sipes5hcx.jpg"
+					}
+				]
+			},
+			{
+				heading: "Architecturals",
+				text: "by Samantha Brooke",
+				images: [
+					{
+						size: "sm",
+						url: "https://res.cloudinary.com/dby4kdmbv/image/upload/v1621241114/photosnap/stories/mobile/gldnkbb531okskljrzuc.jpg"
+					},
+					{
+						size: "md",
+						url: "https://res.cloudinary.com/dby4kdmbv/image/upload/v1621241101/photosnap/stories/desktop/kafh6drtvcnmsmlj8sjc.jpg"
+					},
+					{
+						size: "lg",
+						url: "https://res.cloudinary.com/dby4kdmbv/image/upload/v1621241101/photosnap/stories/desktop/kafh6drtvcnmsmlj8sjc.jpg"
+					}
+				]
+			},
+			{
+				heading: "Calm Waters",
+				text: "by Samantha Brooke",
+				images: [
+					{
+						size: "sm",
+						url: "https://res.cloudinary.com/dby4kdmbv/image/upload/v1621241114/photosnap/stories/mobile/lg0obaa4znqx2wz0fk0g.jpg"
+					},
+					{
+						size: "md",
+						url: "https://res.cloudinary.com/dby4kdmbv/image/upload/v1621241114/photosnap/stories/mobile/lg0obaa4znqx2wz0fk0g.jpg"
+					},
+					{
+						size: "lg",
+						url: "https://res.cloudinary.com/dby4kdmbv/image/upload/v1621241099/photosnap/stories/desktop/y1pooiymdqdkto3zi0ml.jpg"
+					}
+				]
+			},
+			{
+				heading: "Unforeseen Corners",
+				text: "by William Malcolm",
+				images: [
+					{
+						size: "sm",
+						url: "https://res.cloudinary.com/dby4kdmbv/image/upload/v1621241122/photosnap/stories/mobile/papuyo2girqiujovyakn.jpg"
+					},
+					{
+						size: "md",
+						url: "https://res.cloudinary.com/dby4kdmbv/image/upload/v1621241122/photosnap/stories/mobile/papuyo2girqiujovyakn.jpg"
+					},
+					{
+						size: "lg",
+						url: "https://res.cloudinary.com/dby4kdmbv/image/upload/v1621241111/photosnap/stories/desktop/xnscbdnnye3zvrcknvyg.jpg"
+					}
+				]
+			},
+			{
+				heading: "King on Africa: Part II",
+				text: "by Tim Hillenburg",
+				images: [
+					{
+						size: "sm",
+						url: "https://res.cloudinary.com/dby4kdmbv/image/upload/v1621241117/photosnap/stories/mobile/ivnnnhzxoq8rh5gnuled.jpg"
+					},
+					{
+						size: "md",
+						url: "https://res.cloudinary.com/dby4kdmbv/image/upload/v1621241117/photosnap/stories/mobile/ivnnnhzxoq8rh5gnuled.jpg"
+					},
+					{
+						size: "lg",
+						url: "https://res.cloudinary.com/dby4kdmbv/image/upload/v1621241103/photosnap/stories/desktop/v00qpn9w844gvbzpkrr8.jpg"
+					}
+				]
+			},
+			{
+				heading: "The Trip to Nowhere",
+				text: "by Felicia Rourke",
+				images: [
+					{
+						size: "sm",
+						url: "https://res.cloudinary.com/dby4kdmbv/image/upload/v1621241120/photosnap/stories/mobile/ekivkq1pv6ifcv1dtetm.jpg"
+					},
+					{
+						size: "md",
+						url: "https://res.cloudinary.com/dby4kdmbv/image/upload/v1621241120/photosnap/stories/mobile/ekivkq1pv6ifcv1dtetm.jpg"
+					},
+					{
+						size: "lg",
+						url: "https://res.cloudinary.com/dby4kdmbv/image/upload/v1621241109/photosnap/stories/desktop/xy1kvrdomlfeck1ysfxf.jpg"
+					}
+				]
+			},
+			{
+				heading: "Rage of The Sea",
+				text: "by Mohammed Abdul",
+				images: [
+					{
+						size: "sm",
+						url: "https://res.cloudinary.com/dby4kdmbv/image/upload/v1621241118/photosnap/stories/mobile/blb6aogadfchj6c0myrc.jpg"
+					},
+					{
+						size: "md",
+						url: "https://res.cloudinary.com/dby4kdmbv/image/upload/v1621241118/photosnap/stories/mobile/blb6aogadfchj6c0myrc.jpg"
+					},
+					{
+						size: "lg",
+						url: "https://res.cloudinary.com/dby4kdmbv/image/upload/v1621241108/photosnap/stories/desktop/erc9i2dyf0o3qyukgywl.jpg"
+					}
+				]
+			},
+			{
+				heading: "Running Free",
+				text: "by Michelle",
+				images: [
+					{
+						size: "sm",
+						url: "https://res.cloudinary.com/dby4kdmbv/image/upload/v1621241123/photosnap/stories/mobile/jj7vhmwnqlb3y3nb0dsp.jpg"
+					},
+					{
+						size: "md",
+						url: "https://res.cloudinary.com/dby4kdmbv/image/upload/v1621241123/photosnap/stories/mobile/jj7vhmwnqlb3y3nb0dsp.jpg"
+					},
+					{
+						size: "lg",
+						url: "https://res.cloudinary.com/dby4kdmbv/image/upload/v1621241106/photosnap/stories/desktop/ueic3wh0txybjyxu0pjj.jpg"
+					}
+				]
+			},
+			{
+				heading: "Behind the Waves",
+				text: "by Lamarr Wilson",
+				images: [
+					{
+						size: "sm",
+						url: "https://res.cloudinary.com/dby4kdmbv/image/upload/v1621241113/photosnap/stories/mobile/h3ttrv9ac3uj0eoszrit.jpg"
+					},
+					{
+						size: "md",
+						url: "https://res.cloudinary.com/dby4kdmbv/image/upload/v1621241113/photosnap/stories/mobile/h3ttrv9ac3uj0eoszrit.jpg"
+					},
+					{
+						size: "lg",
+						url: "https://res.cloudinary.com/dby4kdmbv/image/upload/v1621241103/photosnap/stories/desktop/ea8tlzpoy06aopgbpofg.jpg"
+					}
+				]
+			},
+			{
+				heading: "Calm Waters",
+				text: "by Samantha Brooke",
+				images: [
+					{
+						size: "sm",
+						url: "https://res.cloudinary.com/dby4kdmbv/image/upload/v1621241114/photosnap/stories/mobile/lg0obaa4znqx2wz0fk0g.jpg"
+					},
+					{
+						size: "md",
+						url: "https://res.cloudinary.com/dby4kdmbv/image/upload/v1621241114/photosnap/stories/mobile/lg0obaa4znqx2wz0fk0g.jpg"
+					},
+					{
+						size: "lg",
+						url: "https://res.cloudinary.com/dby4kdmbv/image/upload/v1621241099/photosnap/stories/desktop/y1pooiymdqdkto3zi0ml.jpg"
+					}
+				]
+			},
+			{
+				heading: "The Milky Way",
+				text: "by Benjamin Cruz",
+				images: [
+					{
+						size: "sm",
+						url: "https://res.cloudinary.com/dby4kdmbv/image/upload/v1621241120/photosnap/stories/mobile/onzvcm74miejrxkrh85q.jpg"
+					},
+					{
+						size: "md",
+						url: "https://res.cloudinary.com/dby4kdmbv/image/upload/v1621241120/photosnap/stories/mobile/onzvcm74miejrxkrh85q.jpg"
+					},
+					{
+						size: "lg",
+						url: "https://res.cloudinary.com/dby4kdmbv/image/upload/v1621241107/photosnap/stories/desktop/vvfhpli86yo9ixrvga9h.jpg"
+					}
+				]
+			},
+			{
+				heading: "Night at The Dark Forest",
+				text: "by  Mohammed Abdul",
+				images: [
+					{
+						size: "sm",
+						url: "https://res.cloudinary.com/dby4kdmbv/image/upload/v1621241117/photosnap/stories/mobile/kh8ggmxqyj4541rnpdxv.jpg"
+					},
+					{
+						size: "md",
+						url: "https://res.cloudinary.com/dby4kdmbv/image/upload/v1621241117/photosnap/stories/mobile/kh8ggmxqyj4541rnpdxv.jpg"
+					},
+					{
+						size: "lg",
+						url: "https://res.cloudinary.com/dby4kdmbv/image/upload/v1621241100/photosnap/stories/desktop/ukgsd7bwxh7jzrhsr4uy.jpg"
+					}
+				]
+			},
+			{
+				heading: "Somwarpet’s Beauty",
+				text: "by Michelle",
+				images: [
+					{
+						size: "sm",
+						url: "https://res.cloudinary.com/dby4kdmbv/image/upload/v1621241120/photosnap/stories/mobile/ehbptui9gwefiqkubemg.jpg"
+					},
+					{
+						size: "md",
+						url: "https://res.cloudinary.com/dby4kdmbv/image/upload/v1621241120/photosnap/stories/mobile/ehbptui9gwefiqkubemg.jpg"
+					},
+					{
+						size: "lg",
+						url: "https://res.cloudinary.com/dby4kdmbv/image/upload/v1621241111/photosnap/stories/desktop/vmy15lhc7ndwhfmig9ua.jpg"
+					}
+				]
+			},
+			{
+				heading: "Land of Dreams",
+				text: "by William Malcolm",
+				images: [
+					{
+						size: "sm",
+						url: "https://res.cloudinary.com/dby4kdmbv/image/upload/v1621241117/photosnap/stories/mobile/j7mmhpjvjq0ahzewvba6.jpg"
+					},
+					{
+						size: "md",
+						url: "https://res.cloudinary.com/dby4kdmbv/image/upload/v1621241117/photosnap/stories/mobile/j7mmhpjvjq0ahzewvba6.jpg"
+					},
+					{
+						size: "lg",
+						url: "https://res.cloudinary.com/dby4kdmbv/image/upload/v1621241104/photosnap/stories/desktop/ogwdjsp7onbj2qh2m25e.jpg"
+					}
+				]
+			}
+		],
+		group3: [
+			{
+				images: [
+					{
+						size: "unique",
+						url: "https://res.cloudinary.com/dby4kdmbv/image/upload/v1621241043/photosnap/features/desktop/pewybacwfmjfigremawr.svg"
+					}
+				],
+				heading: "100% Responsive",
+				text: "No matter which the device you’re on, our site is fully responsive and stories look beautiful on any screen."
+			},
+			{
+				images: [
+					{
+						size: "unique",
+						url: "https://res.cloudinary.com/dby4kdmbv/image/upload/v1621241044/photosnap/features/desktop/zcemjvl8y4mmzu504wlo.svg"
+					}
+				],
+				heading: "No Photo Upload Limit",
+				text: "Our tool has no limits on uploads or bandwidth. Freely upload in bulk and share all of your stories in one go."
+			},
+			{
+				images: [
+					{
+						size: "unique",
+						url: "https://res.cloudinary.com/dby4kdmbv/image/upload/v1621241043/photosnap/features/desktop/kiz1meunvjgwfr6ccrbl.svg"
+					}
+				],
+				heading: "Available to Embed",
+				text: "Embed Tweets, Facebook posts, Instagram media, Vimeo or YouTube videos, Google Maps, and more. "
+			},
+			{
+				images: [
+					{
+						size: "unique",
+						url: "https://res.cloudinary.com/dby4kdmbv/image/upload/v1621241042/photosnap/features/desktop/c4pzkbvm0nwzyuved2cg.svg"
+					}
+				],
+				heading: "Custom Domain",
+				text: "With Photosnap subscriptions you can host your stories on your own domain. You can also remove our branding!"
+			},
+			{
+				images: [
+					{
+						size: "unique",
+						url: "https://res.cloudinary.com/dby4kdmbv/image/upload/v1621241042/photosnap/features/desktop/z3hnyqbtryuedqhzoav0.svg"
+					}
+				],
+				heading: "Boost Your Exposure",
+				text: "Users that viewed your story or gallery can easily get notifed of new and featured stories with our built in mailing list."
+			},
+			{
+				images: [
+					{
+						size: "unique",
+						url: "https://res.cloudinary.com/dby4kdmbv/image/upload/v1621241043/photosnap/features/desktop/ypc2pmxnzxkvssfayzsl.svg"
+					}
+				],
+				heading: "Drag & Drop Image",
+				text: "Easily drag and drop your image and get beautiful shots everytime. No over the top tooling to add friction to creating stories."
+			}
+		],
+		group4: [
+			{
+				heading: "We’re in beta. Get your invite today!",
+				images: [
+					{
+						size: "sm",
+						url: "https://res.cloudinary.com/dby4kdmbv/image/upload/v1621241090/photosnap/shared/mobile/tfqbmtzhyysncwralpfe.jpg"
+					},
+					{
+						size: "md",
+						url: "https://res.cloudinary.com/dby4kdmbv/image/upload/v1621241092/photosnap/shared/tablet/nvqhyeeuhrwcixeqfvit.jpg"
+					},
+					{
+						size: "lg",
+						url: "https://res.cloudinary.com/dby4kdmbv/image/upload/v1621241088/photosnap/shared/desktop/ubzmzkjmhbr16x67cl17.jpg"
+					}
+				]
+			}
+		]
+	}
+};
+
+module.exports = data;
