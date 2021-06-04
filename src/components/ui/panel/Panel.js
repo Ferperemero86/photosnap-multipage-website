9f37c6bf85765c093abf5677bf3d66665994f83f
Ferperemero86 <fernandoperezmerono@gmail.com>
2021-06-04 10:27:35 +0100

    fix panel text

--- src/components/ui/panel/Panel.js
+++ src/components/ui/panel/Panel.js
@@ -4,11 +4,10 @@ import Heading from "../heading/Heading";
 import Button from "../button/Button";
 
 const Panel = ({ stylesClass, heading, bodyText, button, dispatch, price }) => {
-	console.log("price", price);
 	return (
 		<div className={`panel ${stylesClass}`}>
 			<Heading stylesClass="panel-heading" type="h3" text={heading} />
-			<p className="panel-body-text">{bodyText}</p>
+			<p className="panel-text">{bodyText}</p>
 			<p className="panel-price">{price}</p>
 			<Button
 				label={button.label}
