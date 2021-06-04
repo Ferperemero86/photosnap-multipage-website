35898f64b3116193730aaac5b322643d5d607017
Ferperemero86 <fernandoperezmerono@gmail.com>
2021-06-04 10:33:36 +0100

    remove json plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,18 +1,6 @@
-const path = require("path");
-
 module.exports = {
 	siteMetadata: {
 		title: "photosnap-multipage-website"
 	},
-	plugins: [
-		"gatsby-plugin-sass",
-		"gatsby-transformer-json",
-		{
-			resolve: "gatsby-source-filesystem",
-			options: {
-				name: "data",
-				path: path.join(__dirname, "/src/data/")
-			}
-		}
-	]
+	plugins: ["gatsby-plugin-sass"]
 };
